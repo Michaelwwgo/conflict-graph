9aaa2bcbb5ccb7ebdbe6d11009ac76062815431d
unknown <igor.di.paolo@gmail.com>
2015-06-24 02:38:24 +0200

    sup

--- src/main/java/org/idipaolo/cgraph/csv/TopologyExporter.java
+++ src/main/java/org/idipaolo/cgraph/csv/TopologyExporter.java
@@ -1,8 +1,20 @@
 package org.idipaolo.cgraph.csv;
 
+import org.idipaolo.cgraph.graphs.TopologyGraph;
+import org.idipaolo.cgraph.model.Area;
+
 /**
  * Created by Igor on 24/06/2015.
  */
 public class TopologyExporter {
+
+    public void write(TopologyGraph graph,Area area)
+    {
+        //use csv
+        // write obstacles
+        //write links
+
+        //close csv
+    }
 
 }
